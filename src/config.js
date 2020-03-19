838d643f459f2db59f4af42ca3e5c724829a5fda
Deena57017 <Deena57017@gmail.com>
2020-03-19 17:08:13 +0700

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const Config = {
+    apiKey: "AIzaSyD9TX9n2OK6Wr9N5yh0n8PExWBQxm78zt0",
+    authDomain: "todo-727db.firebaseapp.com",
+    databaseURL: "https://todo-727db.firebaseio.com",
+    projectId: "todo-727db",
+    storageBucket: "todo-727db.appspot.com",
+    messagingSenderId: "607731636176",
+    appId: "1:607731636176:web:bea7969d934ca618a2cbd5",
+    measurementId: "G-BLWTS6QPNW"
+  };
+  export default Config;
